85cc7169137f186b03c395cc0bef2946739fe81d
rceglie <rceglie@email.unc.edu>
2022-02-18 19:30:05 -0500

    finally fixed?

--- flips.js
+++ flips.js
@@ -1,6 +1,7 @@
+import minimist from 'minimist'
 import { coinFlips, countFlips } from './modules/coin.mjs'
 
-const args = (process.argv.slice(2))
+const args = minimist(process.argv.slice(2))
 args["number"]
 const number = args.number || 1
 
