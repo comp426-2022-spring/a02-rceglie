67b7a6159239032f16bf61de8e586794948dcff7
rceglie <rceglie@email.unc.edu>
2022-02-18 19:49:07 -0500

    message

--- flips.js
+++ flips.js
@@ -4,12 +4,14 @@ import { coinFlip, coinFlips, countFlips } from './modules/coin.mjs'
 const args = minimist(process.argv.slice(2))
 args["number"]
 const number = args.number || 1
-
+/*
 if (number == 1){
     console.log(coinFlips(number))
 } else {
     var allFlips = coinFlips(number)
     console.log(allFlips)
     console.log(countFlips(allFlips))
-}
-
+}*/
+var allFlips = coinFlips(number)
+console.log(allFlips)
+console.log(countFlips(allFlips))
